0b7e39e625a4d97157e3afcd38880e59b163a89d
Tolga Atalay <recep.tolga.atalay@fu-berlin.de>
2025-12-14 14:13:36 +0100

    update .gitignore

--- stash/run_slit_pore_production.sh
+++
@@ -1,9 +0,0 @@
-halmd Slit-Pore-Production.lua -v \
-    --input /group/ag_compstatphys/data/tolga/simulation/slit_pore__equilibration_D15_rho0.7_T1.00_20251024_180624.h5 \
-    --pore_width 15 \
-    --fluid_density 0.7 \
-    --cutoff 3.5 \
-    --temperature 1.0 \
-    --equilibrium_time 1000 \
-    --slab width 0.25\
-
