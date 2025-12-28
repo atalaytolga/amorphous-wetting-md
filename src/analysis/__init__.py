510246a1eaa3aa8e40fc6752ee214bbd15f56099
Tolga Atalay <recep.tolga.atalay@fu-berlin.de>
2025-12-28 21:24:37 +0100

    Update __init__.py to include new modules
    __init__.py is updated to include newly added modules to analysis
    package.

--- src/analysis/__init__.py
+++ src/analysis/__init__.py
@@ -5,7 +5,8 @@ This package contains analysis code used by scripts and notebooks.
 """
 
 from .paths import PROJECT_ROOT, DATA_DIR, RAW_DIR, PROCESSED_DIR, FIGURES_DIR
-
+from .pbc import wrap_pbc
+from .simulation import Simulation
 
 __all__ = [
     "PROJECT_ROOT",
