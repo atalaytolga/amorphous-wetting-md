ba4b029b892a8296b4bfcf2164b5e4fd52cc5566
Tolga Atalay <recep.tolga.atalay@fu-berlin.de>
2025-12-28 16:02:37 +0100

    Refactor slit_pore_production
    -`Slit pore production` script now calls ssf_analysis definition instead
    of defining in the main function.
    -Removed and renamed input arguments for clarity.

--- scripts/runners/slit_pore_production.lua
+++ scripts/runners/slit_pore_production.lua
@@ -12,6 +12,7 @@ package.path = utility.abspath("../definitions") .. "/?.lua;" .. package.path
 
 local pair_potential = require("pair_potential")
 local slab_analysis = require("slab_analysis")
+local ssf_analysis = require("ssf_analysis")
 
 function read_sample(args)
     -- open H5MD file for reading
@@ -80,7 +81,6 @@ function main(args)
     local file = writers.h5md({path = ("%s.h5"):format(args.output), overwrite = args.overwrite})
 
     pair_potential.create_pair_forces(args.cutoff, args.smoothing, particles, "fluid", "obstacles", box)
-    --ssf_analysis.compute_ssf(args, box, groups, file)
 
 
     local integrator = mdsim.integrators.verlet_nvt_boltzmann({
@@ -90,15 +90,10 @@ function main(args)
         temperature = args.temperature,
         rate = args.rate,
     })
-
-    --[[ add velocity-Verlet integrator with Boltzmann distribution
-    -- add velocity-Verlet integrator (NVE)
-    mdsim.integrators.verlet({particle = particles["fluid"], box = box, timestep = args.timestep})
-    --]]
-    local equilibrium_steps = math.ceil(args.equilibrium_time / args.timestep)
+    local integration_steps = math.ceil(args.time / args.timestep)
 
     -- write phase space trajectory to H5MD file
-    local phase_space_obstacles = observables.phase_space({box = box, group = groups["obstacles"]}):writer({file = file, fields = {"position", "image", "species"}, every = equilibrium_steps})
+    local phase_space_obstacles = observables.phase_space({box = box, group = groups["obstacles"]}):writer({file = file, fields = {"position", "image", "species"}, every = integration_steps})
     local phase_space_fluid     = observables.phase_space({box = box, group = groups["fluid"]}):writer({file = file, fields = {"position", "image", "species"}, every = args.sampling.trajectory})
 
     --write thermodynamic variables to H5MD file
@@ -107,95 +102,27 @@ function main(args)
     every = args.sampling.trajectory})
 
 
-        -- set up wavevectors, compute density modes and static structure factor
-    local interval = args.sampling.structure
-    if interval > 0 or args.sampling.correlation > 0 then
-        -- set up wavevector grid compatible with the periodic simulation box
-        local grid = args.wavevector.wavenumbers
-        if not grid then
-            grid = observables.utility.semilog_grid({
-                start = 2 * math.pi / numeric.max(box.length)
-              , stop = args.wavevector.maximum
-              , decimation = args.wavevector.decimation
-            }).value
-        end
-
-        local wavevector_parallel = observables.utility.wavevector({
-            box = box, wavenumber = grid
-          , tolerance = args.wavevector.tolerance, max_count = args.wavevector.max_count
-          , filter = {1,1,0}
-        })
-
-        local wavevector_perpendicular = observables.utility.wavevector({box = box,
-        wavenumber = grid,
-        dense = true,
-        filter = {0,0,1}
-        })
-
-        local wavevectors = {
-            parallel = wavevector_parallel,
-            perpendicular = wavevector_perpendicular
-        }
-
-
-        -- Compute global density modes
-        local density_mode_parallel_global = observables.density_mode({
-            group = groups["fluid"],
-            wavevector = wavevectors["parallel"]
-        })
-
-        local density_mode_perpendicular_global = observables.density_mode({
-            group = groups["fluid"],
-            wavevector = wavevectors["perpendicular"]
-        })
-
-
-        -- Write global density modes
-        density_mode_parallel_global:writer({
-        file = file,
-        location = {"density_mode", "global_parallel", "bulk"},
-        every = interval
-        })
-
-
-        density_mode_perpendicular_global:writer({
-            file = file,
-            location = {"density_mode", "global_perpendicular", "bulk"},
-            every = interval
-        })
-
-
-        -- Write global structure factors
-        observables.ssf({
-            density_mode = density_mode_parallel_global,
-            norm = groups["fluid"].size
-        }):writer({
-            file = file,
-            location = {"ssf", "global_parallel", "bulk"},
-            every = interval
-        })
-        observables.ssf({
-            density_mode = density_mode_perpendicular_global,
-            norm = groups["fluid"].size
-        }):writer({
-            file = file,
-            location = {"ssf", "global_perpendicular", "bulk"},
-            every = interval
-        })
-        slab_analysis.slab_analysis(args.slab.width, args.slab.axis, particles["fluid"], wavevectors, length, file, box, args)
+    local wavevectors = ssf_analysis.compute_ssf(args, box, groups["fluid"], file)
+    if args.sampling.structure > 0 then
+        slab_analysis.slab_analysis(
+              args.slab.width
+            , args.slab.axis
+            , particles["fluid"]
+            , wavevectors
+            , length
+            , file
+            , box
+            , args
+        )
     end
 
 
     -- sample initial state
     observables.sampler:sample()
     -- estimate remaining runtime
-    observables.runtime_estimate({steps = equilibrium_steps})
+    observables.runtime_estimate({steps = integration_steps})
     -- run simulation
-    observables.sampler:run(equilibrium_steps)
-
-
-
-
+    observables.sampler:run(integration_steps)
 end
 
 function define_args(parser)
@@ -209,14 +136,14 @@ function define_args(parser)
         args[key] = value
     end, help = "H5MD input file"})
 
-    parser:add_argument("overwrite", {type = "boolean", default = true, help = "overwrite output file"})
+    parser:add_argument("overwrite", {type = "boolean", default = false, help = "overwrite output file"})
+
     parser:add_argument('pore_width', {type = 'number', default = 15, help = 'pore width'})
-    parser:add_argument('wall_density', {type = 'number', default = 2.5, help = 'obstacle density'})
     parser:add_argument('fluid_density', {type = 'number', default = 0.7, help = 'fluid density'})
+
     parser:add_argument("temperature", {type = "number", default = 1, help = "target temperature"})
     parser:add_argument("rate", {type = "number", default = 10, help = "heat bath collision rate"})
-    parser:add_argument("equilibrium_time", {type = "number", default = 10, help = "equilibruim integration time"})
-    parser:add_argument("production_time", {type = "number", default = 10, help = "production integration time"})
+    parser:add_argument("time", {type = "number", default = 10, help = "simulation integration time"})
     parser:add_argument("timestep", {type = "number", default = 0.001, help = "integration time step"})
     parser:add_argument("smoothing", {type = "number", default = 0.005, help = "cutoff smoothing parameter"})
     parser:add_argument("cutoff", {type = "float32", default = 3, help = "potential cutoff radius"})
